9f9dd18f83367188c69fb5261eac733593dfcbb1
Crynowecer0 <crynowecer11@gmail.com>
2023-07-27 21:45:33 -0400

    finished modifications for mobile usage

--- static/scripts/utilities.js
+++ static/scripts/utilities.js
@@ -3,3 +3,18 @@ $(document).ready(function(){
       $(this).hide();
   });
 });
+
+$(document).ready(function(){
+  $('#logout').on('click', 'a', function(event){
+      event.preventDefault();
+
+      $.post("/logout", function() {
+          console.log("Logout successful.");
+          // you can redirect to login page or whatever you want after successful logout
+          // window.location.href = "/login";
+      })
+      .fail(function() {
+          console.log("Error on logout.");
+      });
+  });
+});
